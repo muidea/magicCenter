f16bb654061ecdb68f755c6d0f6abd6418bb3682
muidea <muidea@gmail.com>
2016-06-11 21:54:11 +0800

    chage database name

--- src/magiccenter/util/modelhelper/model_helper.go
+++ src/magiccenter/util/modelhelper/model_helper.go
@@ -1,24 +1,24 @@
 package modelhelper
 
 import (
-	"muidea.com/dao"	
+	"muidea.com/dao"
 )
 
 type Model interface {
 	BeginTransaction()
-	
+
 	Commit()
-	
+
 	Rollback()
-	
+
 	Query(string)
-	
+
 	Next() bool
-	
-	GetValue(... interface{})
-	
-	Execute(string) (int64,bool)
-	
+
+	GetValue(...interface{})
+
+	Execute(string) (int64, bool)
+
 	Release()
 }
 
@@ -26,15 +26,15 @@ type model struct {
 	dao *dao.Dao
 }
 
-func NewHelper()(Model, error) {
+func NewHelper() (Model, error) {
 	m := &model{}
-	
-	dao, err := dao.Fetch("root", "rootkit", "localhost:3306", "magicid_db")
+
+	dao, err := dao.Fetch("root", "rootkit", "localhost:3306", "magiccenter_db")
 	if err != nil {
 		return nil, err
 	}
-	
-	m.dao = dao	
+
+	m.dao = dao
 	return m, err
 }
 
@@ -50,23 +50,22 @@ func (this *model) Rollback() {
 	this.dao.Rollback()
 }
 
-func (this *model)Query(sql string) {
+func (this *model) Query(sql string) {
 	this.dao.Query(sql)
 }
 
-func (this *model)Next() bool {
+func (this *model) Next() bool {
 	return this.dao.Next()
 }
 
-func (this *model)GetValue(val ... interface{}) {
+func (this *model) GetValue(val ...interface{}) {
 	this.dao.GetField(val...)
 }
 
-func (this *model)Execute(sql string) (int64, bool) {
+func (this *model) Execute(sql string) (int64, bool) {
 	return this.dao.Execute(sql)
 }
 
-func (this *model)Release() {
+func (this *model) Release() {
 	this.dao.Release()
-} 
-
+}
