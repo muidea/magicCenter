a8cbea0d7c1f7b9bbfd3f23a5ded6b3cdd49134a
rangh <rangh@192.168.0.113>
2016-11-18 00:00:43 +0800

    新增Dao接口

--- src/muidea.com/dao/dao.go
+++ src/muidea.com/dao/dao.go
@@ -1,168 +1,179 @@
 package dao
 
 import (
+	"database/sql"
 	"fmt"
-	"log"	
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
-    "martini"
+	"log"
+	"martini"
+
+	_ "github.com/go-sql-driver/mysql" //引入Mysql驱动
 )
 
-type Dao struct {
-	dbHandle *sql.DB
-	dbTx *sql.Tx
+// Dao 数据库访问对象
+type Dao interface {
+	Release()
+	BeginTransaction()
+	Commit()
+	Rollback()
+	Query(sql string)
+	Next() bool
+	GetField(value ...interface{})
+	Execute(sql string) (int64, bool)
+}
+
+type impl struct {
+	dbHandle   *sql.DB
+	dbTx       *sql.Tx
 	rowsHandle *sql.Rows
 }
 
-func Fetch(user string, password string, address string, dbName string) (*Dao, error) {
-	connectStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", user,password,address,dbName)
-	
+// Fetch 获取一个数据访问对象
+func Fetch(user string, password string, address string, dbName string) (Dao, error) {
+	connectStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", user, password, address, dbName)
+
 	if martini.Env != martini.Prod {
 		log.Print(connectStr)
 	}
-	
-	dao := Dao{dbHandle:nil, dbTx:nil, rowsHandle:nil}
+
+	dao := &impl{dbHandle: nil, dbTx: nil, rowsHandle: nil}
 	db, err := sql.Open("mysql", connectStr)
 	if err != nil {
 		panic("open database exception, err:" + err.Error())
 	} else {
-		dao.dbHandle = db		
+		dao.dbHandle = db
 	}
-	
-	return &dao,err
+
+	return dao, err
 }
 
-func (this *Dao) Release() {
+func (impl *impl) Release() {
 	if martini.Env != martini.Prod {
 		log.Print("close database connection")
 	}
-		
-	if this.rowsHandle != nil {
-		this.rowsHandle.Close()
+
+	if impl.rowsHandle != nil {
+		impl.rowsHandle.Close()
 	}
-	this.rowsHandle = nil
-	
-	if this.dbHandle != nil {
-		this.dbHandle.Close()
+	impl.rowsHandle = nil
+
+	if impl.dbHandle != nil {
+		impl.dbHandle.Close()
 	}
-	this.dbHandle = nil
-		
+	impl.dbHandle = nil
+
 }
 
-func (this *Dao) BeginTransaction() {
+func (impl *impl) BeginTransaction() {
 	if martini.Env != martini.Prod {
 		log.Print("Begin Transaction")
 	}
-	
-	tx, err := this.dbHandle.Begin()
+
+	tx, err := impl.dbHandle.Begin()
 	if err != nil {
 		panic("begin transaction exception, err:" + err.Error())
 	}
-	
-	this.dbTx = tx
+
+	impl.dbTx = tx
 }
 
-func (this *Dao) Commit() {
+func (impl *impl) Commit() {
 	if martini.Env != martini.Prod {
 		log.Print("Commit Transaction")
-	}	
-	
-	if this.dbTx == nil {
+	}
+
+	if impl.dbTx == nil {
 		panic("dbTx is nil")
 	}
-	
-	err := this.dbTx.Commit()
-	if err != nil {		
-		this.dbTx = nil
-		
+
+	err := impl.dbTx.Commit()
+	if err != nil {
+		impl.dbTx = nil
+
 		panic("commit transaction exception, err:" + err.Error())
 	}
-	
-	this.dbTx = nil
+
+	impl.dbTx = nil
 }
 
-func (this *Dao) Rollback() {
+func (impl *impl) Rollback() {
 	if martini.Env != martini.Prod {
 		log.Print("Rollback Transaction")
 	}
-		
-	if this.dbTx == nil {
+
+	if impl.dbTx == nil {
 		panic("dbTx is nil")
 	}
-	
-	err := this.dbTx.Rollback()
-	if err != nil {		
-		this.dbTx = nil
-		
+
+	err := impl.dbTx.Rollback()
+	if err != nil {
+		impl.dbTx = nil
+
 		panic("rollback transaction exception, err:" + err.Error())
 	}
-	
-	this.dbTx = nil
+
+	impl.dbTx = nil
 }
 
-func (this *Dao) Query(sql string) {
-		
-	if this.dbHandle == nil {
+func (impl *impl) Query(sql string) {
+
+	if impl.dbHandle == nil {
 		panic("dbHanlde is nil")
 	}
 
 	if martini.Env != martini.Prod {
 		log.Print("query:" + sql)
 	}
-		
-	rows, err := this.dbHandle.Query(sql)
+
+	rows, err := impl.dbHandle.Query(sql)
 	if err != nil {
 		panic("query exception, err:" + err.Error())
 	}
-	
-	this.rowsHandle = rows
+
+	impl.rowsHandle = rows
 }
 
-func (this *Dao) Next() bool {
-	if this.rowsHandle == nil {
-		panic("rowsHandle is nil");
+func (impl *impl) Next() bool {
+	if impl.rowsHandle == nil {
+		panic("rowsHandle is nil")
 	}
-	
-	ret := this.rowsHandle.Next()
+
+	ret := impl.rowsHandle.Next()
 	if !ret {
-		this.rowsHandle = nil
+		impl.rowsHandle = nil
 	}
-	
+
 	return ret
 }
 
-func (this *Dao) GetField(value ... interface{}){
-	if this.rowsHandle == nil {
-		panic("rowsHandle is nil");
+func (impl *impl) GetField(value ...interface{}) {
+	if impl.rowsHandle == nil {
+		panic("rowsHandle is nil")
 	}
-	
-	err := this.rowsHandle.Scan(value...)
+
+	err := impl.rowsHandle.Scan(value...)
 	if err != nil {
 		panic("scan exception, err:" + err.Error())
 	}
 }
 
-func (this *Dao) Execute(sql string) (int64,bool) {
-	if this.dbHandle == nil {
-		panic("dbHandle is nil");
+func (impl *impl) Execute(sql string) (int64, bool) {
+	if impl.dbHandle == nil {
+		panic("dbHandle is nil")
 	}
-	
+
 	if martini.Env != martini.Prod {
 		log.Print("exec:" + sql)
 	}
-		
-	result, err := this.dbHandle.Exec(sql)
+
+	result, err := impl.dbHandle.Exec(sql)
 	if err != nil {
 		panic("exec exception, err:" + err.Error())
 	}
-	
+
 	num, err := result.RowsAffected()
 	if err != nil {
 		panic("rows affected exception, err:" + err.Error())
 	}
-	
-	return num,true
+
+	return num, true
 }
-
-
-
