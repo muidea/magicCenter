69bbdd7ee914fcaa99ef601e718e5619e34148ee
muidea <rangh@foxmail.com>
2017-07-31 20:13:30 +0800

    update login test script

--- test/LoginTest.py
+++ test/LoginTest.py
@@ -1,33 +1,35 @@
-#!/usr/bin
-#encoding=utf-8
+'LoginTest'
 
 import urllib2
 import urllib
 import cookielib
 
-#MagicCenter magiccenter
 class MagicCenter(object):
+    'MagicCenter'
     def __init__(self):
         self.operate = ''
         self.cookjar = cookielib.LWPCookieJar()
         try:
             self.cookjar.revert('magicCenter.cookie')
-        except Exception, e:
-            print(e)
+        except cookielib.LoadError:
+            pass
+        except IOError:
+            pass
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookjar))
         urllib2.install_opener(self.opener)
 
     def login(self, account, password):
-        params ={'user-account':account,'user-password':password}
-        print('login.....')
-        req = urllib2.Request('http://localhost:8888/cas/user/',urllib.urlencode(params))
+        'login'
+        params = {'user-account': account, 'user-password': password}
+        print 'login.....'
+        req = urllib2.Request('http://localhost:8888/cas/user/', urllib.urlencode(params))
 
         self.operate = self.opener.open(req)
         data = self.operate.readlines()
-        print(data)
+        print data
         self.cookjar.save('magicCenter.cookie')
 
 if __name__ == '__main__':
     TT = MagicCenter()
-    TT.login('rangh@126.com','123')
+    TT.login('rangh@126.com', '123')
     
