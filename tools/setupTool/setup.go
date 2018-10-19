9b5eeb73932ff7e611bb86913476117a98049622
muidea <rangh@foxmail.com>
2018-10-19 20:09:00 +0800

    update setupTool

--- tools/setupTool/setup.go
+++ tools/setupTool/setup.go
@@ -16,12 +16,14 @@ var endpointName = ""
 var endpointID = ""
 var authToken = ""
 var configFile = "config.xml"
+var mode = "init"
 
 func main() {
 	flag.StringVar(&centerServer, "CenterSvr", centerServer, "magicCenter address")
 	flag.StringVar(&endpointName, "EndpointName", endpointName, "endpoint name")
 	flag.StringVar(&endpointID, "EndpointID", endpointID, "endpoint id")
 	flag.StringVar(&authToken, "AuthToken", authToken, "endpoint authtoken")
+	flag.StringVar(&mode, "Mode", mode, "running mode")
 	flag.Parse()
 
 	if len(centerServer) == 0 || len(endpointID) == 0 || len(endpointName) == 0 || len(authToken) == 0 {
@@ -44,7 +46,12 @@ func main() {
 	}
 	defer agent.Stop()
 
-	_, ok = agent.FetchSummary(endpointName, model.CATALOG, authToken, sessionID, nil)
+	rootSumary, ok := agent.FetchSummary(endpointName, model.CATALOG, authToken, sessionID, nil)
+	if mode == "repair" && ok {
+		repairInitContent(&rootSumary, cfg, sessionID, agent)
+		return
+	}
+
 	if ok {
 		return
 	}
@@ -100,4 +107,51 @@ func findCatalog(ids []string, catalogInfo map[string]model.SummaryView) []model
 	}
 
 	return rets
+}
+
+func repairInitContent(rootView *model.SummaryView, cfg *config.Config, sessionID string, agent agent.Agent) {
+	catalogInfo := map[string]model.SummaryView{}
+	articleInfo := map[string]model.SummaryView{}
+
+	for _, val := range cfg.Catalogs {
+		catalogs := findCatalog(val.Catalog, catalogInfo)
+		if len(catalogs) == 0 {
+			catalogs = append(catalogs, *rootView.CatalogUnit())
+		}
+
+		subCatalog, ok := agent.FetchSummary(val.Name, model.CATALOG, authToken, sessionID, &catalogs[0])
+		if ok {
+			catalogInfo[val.ID] = subCatalog
+			continue
+		}
+
+		sub, ok := agent.CreateCatalog(val.Name, val.Description, catalogs, authToken, sessionID)
+		if !ok {
+			log.Printf("create catalog failed, name:%s", val.Name)
+			return
+		}
+
+		catalogInfo[val.ID] = sub
+	}
+
+	for _, val := range cfg.Articles {
+		catalogs := findCatalog(val.Catalog, catalogInfo)
+		if len(catalogs) == 0 {
+			catalogs = append(catalogs, *rootView.CatalogUnit())
+		}
+
+		subArticle, ok := agent.FetchSummary(val.Name, model.ARTICLE, authToken, sessionID, &catalogs[0])
+		if ok {
+			articleInfo[val.ID] = subArticle
+			continue
+		}
+
+		sub, ok := agent.CreateArticle(val.Name, val.Content, catalogs, authToken, sessionID)
+		if !ok {
+			log.Printf("create article failed, name:%s", val.Name)
+			return
+		}
+
+		articleInfo[val.ID] = sub
+	}
 }
